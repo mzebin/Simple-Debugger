a486f09bd9738a214969940366db4caf1241d6fb
mzebin <mohammed.zebin7@gmail.com>
2021-04-26 13:14:47 +0400

    Update: setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="simple-python-debugger",
-    version="0.0.1",
+    version="0.0.2",
     description="Simple Debugger for Python",
     long_description=README,
     long_description_content_type="text/markdown",
